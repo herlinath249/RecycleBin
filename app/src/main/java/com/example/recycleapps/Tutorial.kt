cbc6f8cab712aef53dce27fd126652dd5a467037
herlinath249 <herlina.tri.hapsari@student.uty.ac.id>
2019-12-05 05:25:10 +0700

    Pertemuan 9

---
+++ app/src/main/java/com/example/recycleapps/Tutorial.kt
@@ -0,0 +1,13 @@
+package com.example.recycleapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tutorial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+
+    }
+}
---
+++ app/src/main/java/com/example/recycleapps/kategori.kt
@@ -0,0 +1,12 @@
+package com.example.recycleapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kategori : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kategori)
+    }
+}
